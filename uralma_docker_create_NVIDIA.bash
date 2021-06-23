974882cf82ab1d389bfd867be1b64e0adb3f152a
yottapus <francesco.cerri2@studio.unibo.it>
2021-06-23 14:04:06 +0200

    nvidia

---
+++ uralma_docker_create_NVIDIA.bash
@@ -0,0 +1,15 @@
+docker run -it \
+--network=host \
+--device=/dev/dri
+--gpus all \
+--env="DISPLAY=:1" \
+--env="QT_X11_NO_MITSHM=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="XAUTHORITY=$XAUTH" \
+--volume="$XAUTH:$XAUTH" \
+--env="NVIDIA_VISIBLE_DEVICES=all" \
+--env="NVIDIA_DRIVER_CAPABILITIES=all" \
+--volume="/dev:/dev" \
+--privileged \
+--name uralma  \
+uralma
