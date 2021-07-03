3f531bfe4f330899e41aa9a6c59fa4c6b1d49c69
yottapus <francesco.cerri2@studio.unibo.it>
2021-07-03 15:00:53 +0200

    dep x interfaccia

--- entrypoint/START.sh
+++ entrypoint/START.sh
@@ -3,7 +3,5 @@
 #/bin/bash -c '. /opt/ros/melodic/setup.bash; cd /catkin_ws;catkin_make'
 cd /catkin_ws
 catkin_make
-catkin_make
 echo "source /catkin_ws/devel/setup.bash" >> ~/.bashrc
-#sudo service ssh start #restart
 service ssh start #restart
